a5e5ca90abb9d3008cafa77a75e950ab95311e39
Charlie Nguyen <vawntanng@gmail.com>
2024-09-10 21:55:19 +0700

    [ADD] timeline

--- src/components/home/TimeLine.vue
+++ src/components/home/TimeLine.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
   import { ref } from "vue";
   import Heading from "@/components/common/Heading.vue";
+  import { useI18n } from "vue-i18n";
+
+  const { t } = useI18n();
 
   const timelineArr = ref([
     {
@@ -15,7 +18,7 @@
           chipText: "Personal Project",
           subTitleColor: "#ec6a13",
           subTitle: "Simple Task",
-          content: `Simple Task is a lightweight and efficient task management system designed to help users organize and track their daily tasks with ease. Built using modern technologies such as React, Vite, Node.js, and MongoDB, it offers a fast and intuitive user experience. The system allows users to create, manage, and prioritize tasks, ensuring better productivity and organization. With real-time updates and a clean interface, Simple Task provides a seamless workflow for individuals and teams alike.`
+          content: t("projects.simple-task.description")
         },
         {
           date: "01/2023 – 4/2024",
@@ -24,7 +27,7 @@
           chipText: "Samsung SDS",
           subTitleColor: "#ec6a13",
           subTitle: "Sally",
-          content: `Sally is a Digital Transformation Assessment (DTA) tools for customers. It provides a simple survey for users and serves a simple assessment result. And It supports marketing campaigns to collect and manager customer data.`
+          content: t("projects.sally.description")
         },
         {
           date: "1/2023 - 4/2023",
@@ -33,7 +36,7 @@
           chipText: "Personal Project",
           subTitleColor: "#ec6a13",
           subTitle: "Passo",
-          content: `Passo is a marketing agency platform that offers customizable templates and on-demand design services. It provides a seamless experience for businesses looking to enhance their branding and digital presence, with flexible solutions for template purchases and design rentals`
+          content: t("projects.passo.description")
         },
         {
           date: "1/2023 - 4/2023",
@@ -41,8 +44,8 @@
           chipColor: "#26C",
           chipText: "Personal Project",
           subTitleColor: "#ec6a13",
-          subTitle: "Social Project",
-          content: `The Social Network Platform, built with React.js and Firebase, enables users to connect, share, and engage in real-time. It supports instant messaging, media sharing, and profile management, with Firebase providing fast, secure data storage and authentication`
+          subTitle: "Social Network",
+          content: t("projects.social-network.description")
         }
       ]
     },
@@ -55,14 +58,7 @@
           date: "08/2022 – 12/2022",
           chipText: "CMC Global",
           subTitle: "Monoly",
-          content: `MONOLY is a project about managing task and communication, includes three functions below:
-Discussion: Multi-party Communication
-Cross-company business communication becomes more private, more concise, and faster.
-eSignature: Electronic Document Signing
-Confidentiality is guaranteed as an agreement is shared only between interested parties.
-Drive: Secure File Sharing & Storage
-Share files with anyone without worrying about security and compliance violations.
-`
+          content: t("projects.monoly.description")
         }
       ]
     },
@@ -74,8 +70,8 @@ Share files with anyone without worrying about security and compliance violation
         {
           date: "31/8/2019",
           chipText: "1977Vlog",
-          subTitle: "Actor",
-          content: `<a href='projects.vue'>https://www.1977vlog.com</a>`
+          subTitle: "Actor"
+          // content: `<a href='projects.vue'>https://www.1977vlog.com</a>`
         }
       ]
     }
